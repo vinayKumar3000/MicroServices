379715ae271e0af0740f43bc063cd47809497560
vinayKumar3000 <vurukondavinaykumar@gmail.com>
2022-04-10 18:25:01 +0530

    Added comments about cloud config server

--- spring-cloud-config-server/src/main/java/com/example/cloud/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/example/cloud/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //config server gets higher priority than local application.properties file
+//add spring-cloud-config-server pom.xml
+//add annotation 
+//add git configuration in application.properties 
+//In the client side
+//add spring-cloud-starter-config and spring-cloud-starter-bootstrap pom.xml
+//add spring.config.import=optional:configserver:http://localhost:8012 in application.properties
 @SpringBootApplication
 @EnableConfigServer
 public class SpringCloudConfigServerApplication {
